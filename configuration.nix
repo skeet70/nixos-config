041fefe5b34d25bbe86c2d55d06337190722e796
Murph Murphy <murph@clurictec.com>
2022-10-18 16:19:04 -0600

    Add docker support

--- configuration.nix
+++ configuration.nix
@@ -117,15 +117,8 @@ fonts.enableDefaultFonts = true;
   ];
 
   # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  virtualisation.docker.rootless.enable = true;
+  virtualisation.docker.rootless.setSocketVariable = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
