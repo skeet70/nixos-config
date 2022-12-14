2d525214ebe58e7766037774a01c611e4f52b121
Murph Murphy <murph@clurictec.com>
2022-12-14 13:51:43 -0700

    Working on sway 1.8
    Have to sort out why sending a workspace locks everything up

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,11 +1,25 @@
-_: pkgs: rec {
+self: super: {
   # remove this once 1.8 releases in unstable
-  sway = pkgs.sway.overrideAttrs (old: rec {
-    src = pkgs.fetchFromGitHub {
+  wlroots = super.wlroots.overrideAttrs (old: {
+    # mesonFlags = [ "-Dauto_features=auto" ];
+    src = super.fetchFromGitLab {
+      domain = "gitlab.freedesktop.org";
+      owner = "wlroots";
+      repo = "wlroots";
+      rev = "0.16";
+      sha256 = "sha256-/tyz+w6AP/5quU88KOq2Fhhw8ADtn2oxY9+/ns9jEAU=";
+    };
+    buildInputs = old.buildInputs ++ [ self.hwdata ];
+  });
+
+  sway-unwrapped = super.sway-unwrapped.overrideAttrs (old: rec {
+    version = "1.8-rc3";
+    src = super.fetchFromGitHub {
       owner = "swaywm";
       repo = "sway";
       rev = "1.8-rc3";
       sha256 = "ltmlD8R/Dsv80+f3olHeRFOYlrHdcfMa19CfYADqSfI=";
     };
+    buildInputs = old.buildInputs ++ [ self.pcre2 self.xorg.xcbutilwm.dev ];
   });
 }
